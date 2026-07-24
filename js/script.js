cc4c090dd26e12f3bae6977fa816bd5554360806
José Eduardo <jose-eduardo00@hotmail.com>
2026-07-24 18:15:51 -0300

    Add dark mode toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 
+let escuro = false;
+
+
+function tema() {
+
+    escuro = !escuro;
+
+
+    document.getElementById("body")
+        .classList.toggle("dark");
+
+
+    let imagem = document.getElementById("iconeTema");
+
+
+    if (escuro) {
+
+        imagem.src = "imagens/slime-noite.png";
+
+    } else {
+
+        imagem.src = "imagens/slime-dia (1).png";
+
+    }
+
+}
+
+imagem.classList.add("pular");
+
+setTimeout(() => {
+    imagem.classList.remove("pular");
+}, 500);
