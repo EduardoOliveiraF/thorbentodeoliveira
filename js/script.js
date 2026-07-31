90b1385d2980d1b2fdf0429f6e83c3d1e7dfebdf
José Eduardo <jose-eduardo00@hotmail.com>
2026-07-31 14:08:01 -0300

    Implement theme toggle functionality
    Added a theme toggle function that switches between light and dark modes, updating the icon accordingly.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-<script>
-    function atualizarDataHora() {
-        const agora = new Date();
+let escuro = false;
 
-        // Formata a data (DD/MM/AAAA) e a hora (HH:MM:SS) no padrão brasileiro
-        const dataFormatada = agora.toLocaleDateString('pt-BR');
-        const horaFormatada = agora.toLocaleTimeString('pt-BR');
+function tema() {
 
-        // Atualiza o texto no HTML
-        document.getElementById('data-hora').innerHTML = 
-            `Acessado em: ${dataFormatada} às ${horaFormatada}`;
+    escuro = !escuro;
+
+
+    document.getElementById("body")
+        .classList.toggle("dark");
+
+
+    let imagem = document.getElementById("iconeTema");
+
+
+    if (escuro) {
+
+        imagem.src = "imagens/slime-noite.png";
+
+    } else {
+
+        imagem.src = "imagens/slime-dia (1).png";
+
     }
 
-    // Executa a função imediatamente ao carregar a página
-    atualizarDataHora();
+}
 
-    // Atualiza a cada 1 segundo (1000 milissegundos)
-    setInterval(atualizarDataHora, 1000);
-</script>
+imagem.classList.add("pular");
+
+setTimeout(() => {
+    imagem.classList.remove("pular");
+}, 500);
